e2ad6e9ed1a31bb65c0a2c417c3a76566e3bafaa
simoneautiero <72522659+simoneautiero@users.noreply.github.com>
2024-02-12 00:46:43 +0000

    forms added and feedback implemented

--- src/app/hero.js
+++ src/app/hero.js
@@ -775,18 +775,21 @@ export default function Hero() {
         <form
           name="contact"
           method="POST"
-          action="/"
+          action=""
           netlify
           className="flex gap-x-2 w-2/5"
         >
-          <input
-            type="email"
-            name="email"
-            id="email"
-            className="flex-1 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            placeholder="Enter your email"
-            aria-describedby="email-description"
-          />
+          <label>
+            email
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="flex-1 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="Enter your email"
+              aria-describedby="email-description"
+            />
+          </label>
           <button type="submit" className={styles.notifybtm}>
             Notify me
           </button>
